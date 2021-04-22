25a3b2c15441e8262605b780c610523b72796c08
Vikki Chan <vikkiwchan@gmail.com>
2021-04-22 14:27:15 -0500

    added database

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const app = require('./app');
+const { syncAndSeed } = require('./db');
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
+const init = async () => {
+  await syncAndSeed();
+  app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+  });
+};
+
+init();
